9511c4300507795d9c0bb1adcacee9e616c4502d
Benrski <benjaminarski@gmail.com>
2022-03-13 15:27:05 +0100

    Replaced links with weights (better performance)

--- src/lib/neuralNetwork.ts
+++ src/lib/neuralNetwork.ts
@@ -1,12 +1,6 @@
 import { ActivationFunctionType } from './activationFunction';
-import { newLink, setInput as setLinkInput } from './link';
 import { random } from './math';
-import {
-  mapLinks as mapNeuronLinks,
-  Neuron,
-  newNeuron,
-  predict as neuronPredict,
-} from './neuron';
+import { Neuron, newNeuron, predict as neuronPredict } from './neuron';
 
 export type NeuralNetwork = Neuron[][];
 
@@ -24,36 +18,27 @@ export const newNeuralNetwork = (
   return Array.from({ length: layersCount }, (_, layerIndex) => {
     const { neurons, activationType, inputs } = options[layerIndex];
     const layerId = `${layerIndex + 1}`;
-    const linksCount = inputs ?? options[layerIndex - 1]?.neurons ?? 0;
+    const weightsCount = inputs ?? options[layerIndex - 1]?.neurons ?? 0;
 
     return Array.from({ length: neurons }, (_, neuronIndex) => {
       const neuronId = `${layerId}-${neuronIndex + 1}`;
-      const links = Array.from({ length: linksCount }, (_, linkIndex) => {
-        const linkId = `${neuronId}-${linkIndex + 1}`;
-        return newLink({ id: linkId, weight: random(-1, 1) });
-      });
+      const weights = Array.from({ length: weightsCount }, () => random(-1, 1));
 
-      return newNeuron({ id: neuronId, links, bias: 1, activationType });
+      return newNeuron({ id: neuronId, weights, bias: 1, activationType });
     });
   });
 };
 
-export const predict = (
-  network: NeuralNetwork,
-  inputs: number[]
-): NeuralNetwork =>
-  network.reduce<NeuralNetwork>((updatedNetwork, layer) => {
-    const updatedLayer = layer.map((neuron) => {
-      const updatedNeuron = mapNeuronLinks(neuron, (link, linkIndex) => {
-        const input =
-          updatedNetwork[updatedNetwork.length - 1]?.[linkIndex].output ??
-          inputs[linkIndex];
+export const predict = (network: NeuralNetwork, inputs: number[]): number[] => {
+  const layersOutputs = network.reduce<number[][]>((layersOutputs, layer) => {
+    const layerInputs = layersOutputs[layersOutputs.length - 1] || inputs;
 
-        return setLinkInput(link, input);
-      });
+    const layerOutputs = layer.map((neuron) =>
+      neuronPredict(neuron, layerInputs)
+    );
 
-      return neuronPredict(updatedNeuron);
-    });
-
-    return [...updatedNetwork, updatedLayer];
+    return [...layersOutputs, layerOutputs];
   }, []);
+
+  return layersOutputs[layersOutputs.length - 1] || [];
+};
