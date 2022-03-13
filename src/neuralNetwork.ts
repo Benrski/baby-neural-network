fd6f3b7e9b84d017f1ed1c34757cbbea9f6dde5b
Benrski <benjaminarski@gmail.com>
2022-03-13 14:14:35 +0100

    Added predict function in neural network

--- src/neuralNetwork.ts
+++ src/neuralNetwork.ts
@@ -1,31 +1,58 @@
 import { ActivationFunctionType } from './activationFunction';
-import { newLink } from './link';
+import { newLink, setInput as setLinkInput } from './link';
 import { random } from './math';
-import { Neuron, newNeuron } from './neuron';
+import {
+  mapLinks as mapNeuronLinks,
+  Neuron,
+  newNeuron,
+  predict as neuronPredict,
+} from './neuron';
 
 export type NeuralNetwork = Neuron[][];
 
-export type NeuralNetworkShape = {
+export type NeuralNetworkOptions = {
   neurons: number;
-  activationType: ActivationFunctionType;
+  activationType?: ActivationFunctionType;
+  inputs?: number;
 }[];
 
-export const newNeuralNetwork = (shape: NeuralNetworkShape): NeuralNetwork => {
-  const layersCount = shape.length;
+export const newNeuralNetwork = (
+  options: NeuralNetworkOptions
+): NeuralNetwork => {
+  const layersCount = options.length;
 
   return Array.from({ length: layersCount }, (_, layerIndex) => {
-    const { neurons: neuronsCount, activationType } = shape[layerIndex];
+    const { neurons, activationType, inputs } = options[layerIndex];
     const layerId = `${layerIndex + 1}`;
-    const linksCount = shape[layerIndex - 1]?.neurons ?? 0;
-    const bias = layerIndex > 0 ? 1 : 0;
+    const linksCount = inputs ?? options[layerIndex - 1]?.neurons ?? 0;
 
-    return Array.from({ length: neuronsCount }, (_, neuronIndex) => {
-      const id = `${layerId}-${neuronIndex + 1}`;
-      const links = Array.from({ length: linksCount }, () =>
-        newLink({ weight: random(-1, 1) })
-      );
+    return Array.from({ length: neurons }, (_, neuronIndex) => {
+      const neuronId = `${layerId}-${neuronIndex + 1}`;
+      const links = Array.from({ length: linksCount }, (_, linkIndex) => {
+        const linkId = `${neuronId}-${linkIndex + 1}`;
+        return newLink({ id: linkId, weight: random(-1, 1) });
+      });
 
-      return newNeuron({ id, links, bias, activationType });
+      return newNeuron({ id: neuronId, links, bias: 1, activationType });
     });
   });
 };
+
+export const predict = (
+  network: NeuralNetwork,
+  inputs: number[]
+): NeuralNetwork =>
+  network.reduce<NeuralNetwork>((updatedNetwork, layer) => {
+    const updatedLayer = layer.map((neuron) => {
+      const updatedNeuron = mapNeuronLinks(neuron, (link, linkIndex) => {
+        const input =
+          updatedNetwork[updatedNetwork.length - 1]?.[linkIndex].output ??
+          inputs[linkIndex];
+        return setLinkInput(link, input);
+      });
+
+      return neuronPredict(updatedNeuron);
+    });
+
+    return [...updatedNetwork, updatedLayer];
+  }, []);
