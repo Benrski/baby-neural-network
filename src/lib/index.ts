16676de6c1bae3e21e748f77bb828079f84e2388
Benrski <benjaminarski@gmail.com>
2022-03-13 18:02:18 +0100

    Changed lib exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,18 @@
-export {
+import {
   ActivationFunctionType,
   ACTIVATION_FUNCTIONS,
 } from './activationFunction';
-export { ErrorFunctionType, ERROR_FUNCTIONS } from './errorFunction';
-export { newNeuralNetwork, predict } from './neuralNetwork';
+import { ErrorFunctionType, ERROR_FUNCTIONS } from './errorFunction';
+import { newNeuralNetwork, predict } from './neuralNetwork';
+
+export { ActivationFunctionType, ACTIVATION_FUNCTIONS };
+export { ErrorFunctionType, ERROR_FUNCTIONS };
+export { newNeuralNetwork, predict };
+export default {
+  ActivationFunctionType,
+  ACTIVATION_FUNCTIONS,
+  ErrorFunctionType,
+  ERROR_FUNCTIONS,
+  newNeuralNetwork,
+  predict,
+};
